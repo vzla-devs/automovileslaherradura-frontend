92839d2891116682a8c118b521e574cc323efd70
Gabriel Nuñez <gabriel.nzn@gmail.com>
2022-10-09 19:39:29 +0100

    changed html elements

--- automovileslaherradura-frontend/pages/index.js
+++ automovileslaherradura-frontend/pages/index.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="w-full h-[100vh] grid grid-cols-1 grid-rows-12 grid-flow-row items-center">
-      <div className='w-full h-full row-span-1'>
+      <header className='w-full h-full row-span-1'>
         <div className="w-full h-full flex items-center justify-center">
           <Image
             src={Logo}
@@ -13,17 +13,17 @@ export default function Home() {
             alt="logo"
           />
         </div>
-      </div>
+      </header>
       <div className="bg-[#00378a] p-4 row-span-10 w-full h-full flex flex-col items-center justify-center">
-        <p className="text-white text-xl font-bold">
+        <h1 className="text-white text-xl font-bold">
           EN MANTENIMIENTO
-        </p>
+        </h1>
         <p className="text-white text-lg text-center">
           Actualmente estamos realizando labores de mantenimiento <br />
           y mejoras a nuestra página web.
         </p>
       </div>
-      <div className='footer m-4 row-span-1 w-full h-[80px] gap-2 flex flex-col items-center justify-center'>
+      <footer className='footer m-4 row-span-1 h-[80px] gap-2 flex flex-col items-center justify-center'>
         <div className='flex items-center justify-center gap-2'>
           <p className="text-[#00378a] font-bold">
             Dirección:
@@ -48,7 +48,7 @@ export default function Home() {
             automovileslaherradura@gmail.com
           </p>
         </div>
-      </div>
+      </footer>
     </div>
   )
 }
