db58e046f5e285056381f94f785f747f971c40fd
Gabriel Nuñez <gabriel.nzn@gmail.com>
2022-10-14 17:54:02 +0100

    added storyblok configuration and env variable

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    storyblokApiToken: 'STORYBLOK_API_TOKEN',
+  },
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/globals.css'
 
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+ 
+storyblokInit({
+  accessToken: process.env.storyblokApiToken,
+  use: [apiPlugin]
+})
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
