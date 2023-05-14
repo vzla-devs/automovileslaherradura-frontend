0e3185529a2d68d213d8ab0ed628106b686f5096
Paola Parra <adapparrad@gmail.com>
2023-05-14 17:48:37 +0100

    cambios

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -39,7 +39,7 @@ const Layout = ({ children }) => {
             </a>
           </address>
         </div>
-        <div className="md:hidden">
+        <div className="md:hidden ">
           <a href="tel:+603552656" className="bg-[#00378a] text-white rounded-full p-1 flex ml-3 gap-2 cursor-pointer">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
