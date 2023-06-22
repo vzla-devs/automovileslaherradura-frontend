6c0f624fd7e7cca8fb0640af3aaa31e6cd1c3591
VictorR29 <victormramonesd@gmail.com>
2023-06-22 09:48:19 -0500

    adding footer logo

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Logo from "../../assets/logo.jpg"
+import LogoFooter from "../../assets/logo-footer.png";
 
 const Layout = ({ children }) => {
   return (
